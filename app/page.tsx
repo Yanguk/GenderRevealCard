568b14a899b8b36e2b7f4fb9d2167c561bcfd729
Yanguk <yanguk@duck.com>
2025-09-30 21:43:33 +0900

    update card
    Signed-off-by: Yanguk <yanguk@duck.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import frontCard from "../public/back3.png";
-import headerCard from "../public/front3.png";
+import frontCard from "../public/2.png";
+import bodyCard from "../public/layer-bg.png";
+import headerCard from "../public/1.png";
 import Image from "next/image";
 import { FireworksBackground } from "@/components/FireWorks";
 import { useEffect, useRef, useState } from "react";
@@ -40,7 +41,7 @@ export default function Home() {
     }
   }, [open]);
 
-  const gender = "M" as "M" | "W";
+  const gender = Math.random() * 10 < 5 ? "M" : "W"  as "M" | "W";
 
   return (
     <div className="relative max-w-4xl h-dvh m-auto flex flex-col justify-center">
@@ -54,30 +55,39 @@ export default function Home() {
           몰리의 성별은?
         </h1>
 
+        <Image
+          className="absolute top-0"
+          src={bodyCard}
+          alt="card"
+          width={320}
+          height={210}
+          priority
+        />
+
         <div className="size-full bg-[#587389]" />
 
         <div
           className={cn(
-            "absolute bg-white w-3/4 h-6/7 rounded-2xl -translate-x-1/2 left-1/2 -translate-y-1/2 -bottom-18 z-20 transition flex flex-col items-center justify-center gap-[3px] shadow p-[3px] text-center",
+            "absolute w-3/4 h-6/7 rounded-2xl -translate-x-1/2 left-1/2 -translate-y-1/2 -bottom-18 z-20 transition flex flex-col items-center justify-center gap-[3px] shadow p-[3px] text-center",
             !isLoadComplete && "hidden",
             moveCard && "-translate-y-65 duration-800",
-            gender === "W" ? "bg-pink-200" : "bg-blue-200",
+            gender === "W" ? "bg-[#FCD9D9]" : "bg-[#C3DEFC]",
           )}
         >
           {gender === "W" ? (
             <>
-              <h1 className="text-[46px]">👧</h1>
+              <h1 className="text-[46px]">👧🏻</h1>
               <h3 className="text-[20px] font-semibold">딸이에요!</h3>
               <p className="text-balance break-words break-keep text-[18px]">
-                사랑스러운 공주님을 만나게 되실 거예요 💕
+                사랑스러운 공주님을 <br /> 만나게 되실 거예요 💕
               </p>
             </>
           ) : (
             <>
-              <h1 className="text-[46px]">👦</h1>
+              <h1 className="text-[46px]">👦🏻</h1>
               <h3 className="text-[20px] font-semibold">아들이에요!</h3>
               <p className="text-balance break-words break-keep text-[18px]">
-                멋진 왕자님을 만나게 되실 거예요 💙
+                멋진 왕자님을 <br /> 만나게 되실 거예요 💙
               </p>
             </>
           )}
@@ -105,6 +115,10 @@ export default function Home() {
           alt="Card"
           width={320}
           height={186}
+          style={{
+            height: "auto",
+            width: 'auto'
+          }}
           onLoad={() => setImageTwoLoaded(true)}
           priority
         />
@@ -113,7 +127,7 @@ export default function Home() {
       {moveCard && (
         <FireworksBackground
           className="absolute inset-0 flex items-center justify-center rounded-xl"
-          population={3}
+          population={1}
         />
       )}
     </div>

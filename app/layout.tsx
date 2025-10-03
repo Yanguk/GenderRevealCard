ec0a9341dd6ac8226906c75225a060a3586f103b
Yanguk <yanguk@duck.com>
2025-10-03 16:20:18 +0900

    og 속성 추가
    Signed-off-by: Yanguk <yanguk@duck.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +15,18 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "몰리의 성별은?",
   description: "두근 두근",
+  openGraph: {
+    title: "몰리의 성별은?",
+    description: "두근 두근",
+    type: "website",
+    url: "https://gender-reveal-card.vercel.app",
+    images: [
+      {
+        url: "/thumbnail.png",
+        alt: "몰리",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,12 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <Head>
-        <title>몰리의 성별은?</title>
-        <meta property="og:title" content="몰리의 성별은?" />
-        <meta property="og:description" content="두근 두근" />
-        <meta property="og:image" content="" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-300/30`}
       >
